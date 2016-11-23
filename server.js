71ea4931a43330dfa0779820acd42d31261756d6
nico@yoursole.com <nicoflojo.developer@gmail.com>
2016-11-23 11:08:30 -0800

    finished scraper

--- server.js
+++ server.js
@@ -1,15 +1,42 @@
 var express = require('express');
-var fs = require('fs');
+var fs      = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
+var app     = express();
 
-var app = express();
+app.get('/scrape', function(req, res){
+  url = 'http://www.imdb.com/title/tt1229340/';
 
-app.get('/scrape', function(req, res) {
+  request(url, function(error, response, html){
+    if(!error){
+      var $ = cheerio.load(html);
 
+      var title, release, rating;
+      var json = { title : "", release : "", rating : ""};
+
+      $('.title_wrapper').filter(function(){
+        var data = $(this);
+        title = data.children().first().text().trim();
+        release = data.children().last().children().last().text().trim();
+        json.title = title;
+        json.release = release;
+      })
+
+      $('.ratingValue').filter(function(){
+        var data = $(this);
+        rating = data.text().trim();
+        json.rating = rating;
+      })
+    }
+
+    fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
+      console.log('File successfully written! - Check your project directory for the output.json file');
+    })
+
+    res.send('Check your console!')
+  })
 })
 
-app.listen('2003');
-console.log("magic happens on 2003");
-
+app.listen('8081')
+console.log('Magic happens on port 8081');
 exports = module.exports = app;
